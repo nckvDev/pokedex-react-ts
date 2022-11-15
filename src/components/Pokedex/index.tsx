9b4eb32562503796e39c3cc154780c7d166d9baa
nack <jirapong.k@touchtechnologies.co.th>
2022-11-15 14:57:16 +0700

    feat: pagination and error message

--- src/components/Pokedex/index.tsx
+++ src/components/Pokedex/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import * as C from './styles'
 import { Pokemon } from '../../types/Pokemon'
 import { PokemonCard } from '../PokemonCard'
+import { ErrorMessage } from '../helper/ErrorMessage'
+import { Loading } from '../helper/Loading'
+import UsePagination from '../Pagination'
 
 type PokedexProps = {
   setModal: (value: boolean) => void
@@ -22,20 +25,37 @@ type PokedexProps = {
 }
 
 export const Pokedex = (props: PokedexProps) => {
-  return (
+  return props.error ? (
+    <ErrorMessage />
+  ) : (
     <C.Wrapper>
-      Pokedex
       <div className='main-container'>
-        <C.PokemonList>
-          {props.pokemonList.map(pokemon => (
-            <PokemonCard
-              key={pokemon.id}
-              pokemon={pokemon}
-              setModal={props.setModal}
-              setPokemonData={props.setPokemonData}
-            />
-          ))}
-        </C.PokemonList>
+        {props.loading ? (
+          <Loading />
+        ) : (
+          <C.PokemonList>
+            {props.pokemonList.map(pokemon => (
+              <PokemonCard
+                key={pokemon.id}
+                pokemon={pokemon}
+                setModal={props.setModal}
+                setPokemonData={props.setPokemonData}
+              />
+            ))}
+          </C.PokemonList>
+        )}
+        {props.pokemonList.length > 1 && props.loading === false && props.showPagination === true && (
+          <UsePagination
+            setPokemonList={props.setPokemonList}
+            setLoading={props.setLoading}
+            searchBarRef={props.searchBarRef}
+            page={props.page}
+            setPage={props.setPage}
+          />
+        )}
+        {props.pokemonList.length > 1 && props.loading === false && props.showPagination === true && (
+          <C.ButtonContainer></C.ButtonContainer>
+        )}
       </div>
     </C.Wrapper>
   )
